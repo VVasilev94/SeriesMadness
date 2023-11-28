f79ec5d8f4c1784e598361fd6483329c1f867b2f
Vasil Vasilev <vasilvasilev944@gmail.com>
2023-11-28 23:33:38 +0200

    react-bootstrap removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 import Header from "./components/header/Header"
 import Home from './components/home/Home';
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,25 +1,24 @@
 import { Link } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+
 
 
 export default function Header() {
     return(
-        <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Series Madness</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">All Series</Nav.Link>
-            <Nav.Link href="#link">Add new series</Nav.Link>
-            <Nav.Link href="#link">Login</Nav.Link>
-            <Nav.Link href="#link">Register</Nav.Link>
-            <Nav.Link href="#link">Logout</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <header>
+      <h1><Link to="/">SeriesMadness</Link></h1>
+      <nav>
+          <Link to="/series">SeriesCollection</Link>
+         <div id="user">
+                  <Link to="/series/create">Add New Series</Link>
+                  <Link to="/logout">Logout</Link>
+              </div>
+          
+              <div id="guest">
+                  <Link to="/login">Login</Link>
+                  <Link to="/register">Register</Link>
+              </div>
+          
+      </nav>
+  </header>
     );
 }
