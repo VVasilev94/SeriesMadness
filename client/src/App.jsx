42a336a6775e8a801d92ed746c95204ba62b02cf
Vasil Vasilev <vasilvasilev944@gmail.com>
2023-11-30 02:07:05 +0200

    apply basic css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,31 @@
 import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/authContext';
-
-import Header from "./components/header/Header"
+import Header from "./components/header/Header";
 import Home from './components/home/Home';
 import SeriesCollection from './components/series-collection/SeriesCollection';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
 import AddNewSeries from './components/addNewSeries/AddNewSeries';
 
-function App() {
+export function App() {
 
   return (
     <AuthProvider>
-    <div id="main">
       <Header />
-    
-    <Routes>
-     <Route path='/' element={<Home/>} />
-     <Route path='/series' element={<SeriesCollection/>} />
-     <Route path='/login' element={<Login/>} />
-     <Route path='/register' element={<Register/>} />
-     <Route path='/series/create' element={<AddNewSeries/>} />
+      <div id="main-section">
 
-    </Routes>
-    </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/series' element={<SeriesCollection />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/series/create' element={<AddNewSeries />} />
+
+        </Routes>
+      </div>
+      
+
+
     </AuthProvider>
-  )
+  );
 }
-
-export default App
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,22 +2,35 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
-      <h1>
-        <Link to="/">SeriesMadness</Link>
-      </h1>
-      <nav>
-        <Link to="/series">SeriesCollection</Link>
-        <div id="user">
-          <Link to="/series/create">Add New Series</Link>
-          <Link to="/logout">Logout</Link>
-        </div>
+    <header className="header-section">
 
-        <div id="guest">
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </div>
-      </nav>
+      <div className="container">
+
+          <div className="logo-wrapper">
+            <Link to="/">SeriesMadness</Link>
+          </div>
+
+          <div className="navigation-wrapper"> 
+
+            <nav className="navigation-container">
+              <div className="dashboard-wrapper">
+                <Link to="/series">SeriesCollection</Link>
+              </div>
+
+              <div className="user-wrapper">
+                <Link to="/series/create">Add New Series</Link>
+                <Link to="/logout">Logout</Link>
+              </div>
+
+              <div className="guest-wrapper">
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+              </div>
+
+            </nav>
+
+          </div>
+      </div>
     </header>
   );
 }
