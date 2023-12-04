cbafbc653e0fa1dffed4849325bc7a9b849d3685
Vasil Vasilev <vasilvasilev944@gmail.com>
2023-12-04 17:41:54 +0200

    modify create functionality

--- client/src/components/addNewSeries/AddNewSeries.jsx
+++ client/src/components/addNewSeries/AddNewSeries.jsx
@@ -1,7 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+
+import * as seriesService from '../../services/seriesService';
+
+import './addNewSeries.css'
+
+
+
 export default function AddNewSeries() {
+    const navigate = useNavigate();
+    
+    const createSerieSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        const seriesData = Object.fromEntries(new FormData(e.currentTarget));
+
+        try {
+            await seriesService.create(seriesData);
+
+            navigate('/series');
+        } catch (err) {
+            // Error notification
+            console.log(err);
+        }
+    }
     return(
         <section id="create-page" className="auth">
-            <form id="create">
+            <form id="create" onSubmit={createSerieSubmitHandler}>
                 <div className="container">
                     <h1>Add New Series</h1>
                     <label htmlFor="leg-title">Title:</label>
@@ -11,10 +35,7 @@ export default function AddNewSeries() {
                     <input type="number" id="year" name="year" placeholder="Enter series release year..." />
 
                     <label htmlFor="genres">Genres:</label>
-                    <input type="text" id="genres" name="genres" min="1" placeholder="Enter genres..." />
-
-                    <label htmlFor="description">Starring:</label>
-                    <input type="text" id="description" name="description" placeholder="Enter show cast..." />
+                    <input type="text" id="genres" name="genres" placeholder="Enter genres..." />
 
                     <label htmlFor="imageUrl">imageUrl:</label>
                     <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
@@ -24,7 +45,7 @@ export default function AddNewSeries() {
 
                     <label htmlFor="description">Description:</label>
                     <textarea name="description" id="description"></textarea>
-                    <input className="btn submit" type="submit" value="Create Game" />
+                    <input className="btn submit" type="submit" value="Create Serie" />
                 </div>
             </form>
         </section>
