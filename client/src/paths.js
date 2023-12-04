b48ebb5bddef98e22d0add26c58485528a193e98
Vasil Vasilev <vasilvasilev944@gmail.com>
2023-12-04 17:42:47 +0200

    add hoc and paths

---
+++ client/src/paths.js
@@ -0,0 +1,9 @@
+const Path = {
+    Home: '/',
+    Logout: '/logout',
+    SerieDetails: '/series/:serieId',
+    SerieEdit: '/series/:serieId/edit',
+    SerieDelete: '/series/:serieId/delete',
+};
+
+export default Path;
