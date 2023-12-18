4154c04f3f4346f7dd05e2bda2310f3063c4da26
Vasil Vasilev <vasilvasilev944@gmail.com>
2023-12-18 18:10:36 +0200

    edit About page

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,7 +1,14 @@
+import React from 'react'
+import './about.css'
+
 export default function About() {
     return(
-        <div>
-            ABOUT
-        </div>
+        <div className="about-page">
+        <h1 className='about-title'>Welcome to Series Madness!</h1>
+        <p>We're your go-to app for everything television. Whether you're a binge-watcher, a series connoisseur, or just seeking your next favorite show, we've got you covered.</p>
+        <p>Dive into our curated collection of TV series, spanning genres, eras, and global favorites. Our intuitive interface makes discovering, tracking, and exploring new shows a breeze.</p>
+        <p>Uncover hidden gems, catch up on trending series, and create your personalized watchlist. With TV Shows Little, your entertainment journey is just a tap away.</p>
+        <p>Get ready to immerse yourself in a world of captivating stories, unforgettable characters, and endless entertainment!</p>
+      </div>
     )
 }
